1ca4490c55508e418b55dd2f498408b2dbca17a9
mintoya <hfm598@gmail.com>
2024-12-17 17:21:40 -0600

    - fixed color switching - added even more colors

---
+++ lua/mapping.lua
@@ -0,0 +1,35 @@
+keymaps = {
+    {"n", ";", ":", {noremap = false, silent = false}},
+    {"n", "y", '"+y', {noremap = true, silent = true}},
+    {"v", "y", '"+y', {noremap = true, silent = true}},
+    {"n", "s", ":write<CR>", {noremap = true, silent = true}},
+    {
+        "n",
+        "<leader>c",
+        ":Telescope themes<CR>",
+        {desc = "telescope colorschemes", noremap = true, silent = true}
+    },
+    {
+        "n",
+        "<leader>ff",
+        ":Telescope find_files<CR>",
+        {desc = "find files", noremap = true, silent = true}
+    },
+    {
+        "n",
+        "<leader>ft",
+        ":Telescope live_grep<CR>",
+        {desc = "find text in files", noremap = true, silent = true}
+    },
+    {"n", "<leader>b", ":tabnew<CR>", {noremap = true, silent = true}},
+    {"n", "<leader>e", ":Neotree focus<CR>", {noremap = true, silent = true}},
+    {"n", "<leader>x", ":bd<CR>", {desc = "close buffer", noremap = true, silent = true}},
+    {"n", "<C-n>", ":Neotree<CR>", {noremap = true, silent = true}}
+}
+for _, keymap in ipairs(keymaps) do
+    local mode = keymap[1]
+    local lhs = keymap[2]
+    local rhs = keymap[3]
+    local opts = keymap[4]
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
