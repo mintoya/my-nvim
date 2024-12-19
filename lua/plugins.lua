84ac85a77933a4594ed7e776bc05d2fe7cb7fd79
mintoya <hfm598@gmail.com>
2024-12-18 22:54:08 -0600

    even more plugins
    snacks.nvim and mini.nvim added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,8 +10,60 @@ local lspOptions = {
       lsp_fallback = true,
    },
 }
+local snacksConfig = {
+   styles = {
+      position = "float",
+      backdrop = 60,
+      height = 0.9,
+      width = 0.9,
+      zindex = 50,
+   },
+   lazygit = {},
+   notifier = {},
+   win = {
+      position = "float",
+      backdrop = 60,
+      height = 0.9,
+      width = 0.9,
+      zindex = 50,
+   },
+} -- doesnt work >:(
+local miniConfig = function()
+   require('mini.completion').setup({
+   })
+   require('mini.comment').setup({
+      -- mappings = {
+      --    comment = '<leader>/'
+      --    }
+      mappings = {
+         -- Toggle comment (like `gcip` - comment inner paragraph) for both
+         -- Normal and Visual modes
+         comment = 'gc',
 
+         -- Toggle comment on current line
+         comment_line = '<leader>/',
+
+         -- Toggle comment on visual selection
+         comment_visual = '<leader>/',
+
+         -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+         -- Works also in Visual mode if mapping differs from `comment_visual`
+         textobject = 'gc',
+      },
+   })
+   require("mini.statusline").setup({
+      section = {
+         left = function()
+            return '%f %y' -- Displays the filename and filetype
+         end,
+         right = function()
+            return '%l:%c %p%%'
+         end,
+      },
+   })
+end
 local plugins = {
+   { 'folke/snacks.nvim',    opts = snacksConfig },
    {
       "williamboman/mason.nvim",
       config = function()
@@ -24,33 +76,11 @@ local plugins = {
    {
       'echasnovski/mini.nvim',
       version = false,
-      config = function()
-         require("mini.statusline").setup({
-            section = {
-               left = function()
-                  return '%f %y' -- Displays the filename and filetype
-               end,
-               right = function()
-                  return '%l:%c %p%%' -- Displays line number, column number, and percentage
-               end,
-            },
-         })
-      end
+      config = miniConfig
    },
    { 'rainglow/vim',          as = 'rainglow' },
    { "folke/tokyonight.nvim", opts = { style = "storm" } },
    { "neovim/nvim-lspconfig" },
-   {
-      'numToStr/Comment.nvim',
-      config = function()
-         require('Comment').setup({
-            toggler = {
-               line = '<leader>/',
-            },
-            opleader = { line = '<leader>/' }
-         })
-      end
-   },
    { 'stevearc/conform.nvim', opts = lspOptions },
    { "hrsh7th/nvim-cmp" },
    {
@@ -115,5 +145,4 @@ local plugins = {
       end
    }
 }
-
 return plugins
