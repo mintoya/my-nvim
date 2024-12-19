09b78b686da77e889d78468d390809cfb00aa557
mintoya <hfm598@gmail.com>
2024-12-18 21:51:31 -0600

    plugins moved
    moved configs into lua/plugins

--- init.lua
+++ init.lua
@@ -11,19 +11,6 @@ vim.opt.expandtab = true
 
 
 require("lazy").setup(require('plugins'))
-
-require("mini.statusline").setup({
-   section = {
-      left = function()
-         return '%f %y' -- Displays the filename and filetype
-      end,
-      right = function()
-         return '%l:%c %p%%' -- Displays line number, column number, and percentage
-      end,
-   },
-})
-
 require('mapping')       --enables coustom keybinds
 require('current-theme') --required to remember theme
 require('lsps')
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,3 @@
-
 local lspOptions = {
    formatters_by_ft = {
       lua = { "stylua" },
@@ -13,89 +12,108 @@ local lspOptions = {
 }
 
 local plugins = {
-      {"williamboman/mason.nvim",config = function ()
+   {
+      "williamboman/mason.nvim",
+      config = function()
          require("mason").setup()
-      end},
-      { 'L3MON4D3/LuaSnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'neovim/nvim-lspconfig' },
-      { 'echasnovski/mini.nvim', version = false },
-      { 'rainglow/vim',          as = 'rainglow' },
-      { "folke/tokyonight.nvim", opts = { style = "storm" } },
-      { "neovim/nvim-lspconfig" },
-      {
-         'numToStr/Comment.nvim',
-         config = function()
-            require('Comment').setup({
-               toggler = {
-                  line = '<leader>/',
+      end
+   },
+   { 'L3MON4D3/LuaSnip' },
+   { 'hrsh7th/cmp-nvim-lsp' },
+   { 'neovim/nvim-lspconfig' },
+   {
+      'echasnovski/mini.nvim',
+      version = false,
+      config = function()
+         require("mini.statusline").setup({
+            section = {
+               left = function()
+                  return '%f %y' -- Displays the filename and filetype
+               end,
+               right = function()
+                  return '%l:%c %p%%' -- Displays line number, column number, and percentage
+               end,
+            },
+         })
+      end
+   },
+   { 'rainglow/vim',          as = 'rainglow' },
+   { "folke/tokyonight.nvim", opts = { style = "storm" } },
+   { "neovim/nvim-lspconfig" },
+   {
+      'numToStr/Comment.nvim',
+      config = function()
+         require('Comment').setup({
+            toggler = {
+               line = '<leader>/',
+            },
+            opleader = { line = '<leader>/' }
+         })
+      end
+   },
+   { 'stevearc/conform.nvim', opts = lspOptions },
+   { "hrsh7th/nvim-cmp" },
+   {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.8",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+         require("telescope").setup {
+            defaults = {
+               layout_strategy = "flex",
+               vimgrep_arguments = {
+                  "rg",
+                  "--no-heading",    -- No headings in the result
+                  "--with-filename", -- Show filenames
+                  "--line-number",   -- Show line numbers
+                  "--column"         -- Show column numbers
                },
-               opleader = { line = '<leader>/' }
-            })
-         end
-      },
-      { 'stevearc/conform.nvim', opts = lspOptions },
-      { "hrsh7th/nvim-cmp" },
-      {
-         "nvim-telescope/telescope.nvim",
-         tag = "0.1.8",
-         dependencies = { "nvim-lua/plenary.nvim" },
-         config = function()
-            require("telescope").setup {
-               defaults = {
-                  layout_strategy = "flex",
-                  vimgrep_arguments = {
-                     "rg",
-                     "--no-heading",    -- No headings in the result
-                     "--with-filename", -- Show filenames
-                     "--line-number",   -- Show line numbers
-                     "--column"         -- Show column numbers
+               pickers = {
+                  colorscheme = {
+                     enable_preview = true,
                   },
-                  pickers = {
-                     colorscheme = {
-                        enable_preview = true,
-                     },
-                     file_files = {
-                        enable_preview = true,
-                        previewer = require("telescope.previewers").vim_buffer_cat.new
-                     }
+                  file_files = {
+                     enable_preview = true,
+                     previewer = require("telescope.previewers").vim_buffer_cat.new
                   }
                }
             }
-         end
-      },
-      {
-         "nvim-neo-tree/neo-tree.nvim",
-         dependencies = { "MunifTanjim/nui.nvim", 'andrew-george/telescope-themes', 'nvim-tree/nvim-web-devicons' },
-         opts = {
-            filesystem = {
-               filtered_items = {
-                  visible = true,
-               }
-            },
-            window = {
-               width = 20,
-            },
          }
-      },
-      {
-         "folke/which-key.nvim",
-         event = "VeryLazy",
-         keys = {
-            {
-               "<leader>?",
-               function()
-                  require("which-key").show({ global = false })
-               end,
-               desc = "Buffer Local Keymaps (which-key)"
+      end
+   },
+   {
+      "nvim-neo-tree/neo-tree.nvim",
+      dependencies = { "MunifTanjim/nui.nvim", 'andrew-george/telescope-themes', 'nvim-tree/nvim-web-devicons' },
+      opts = {
+         filesystem = {
+            filtered_items = {
+               visible = true,
             }
-         }
-      },
-      {
-         'goolord/alpha-nvim',
-         config = function()
-            require 'alpha'.setup(require('welcome').config)
-         end
+         },
+         window = {
+            width = 20,
+         },
       }
+   },
+   {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      keys = {
+         {
+            "<leader>?",
+            function()
+               require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)"
+         }
+      }
+   },
+   {
+      'goolord/alpha-nvim',
+      config = function()
+         require 'alpha'.setup(require('welcome').config)
+      end
    }
+}
+
 return plugins
