91a793a84c3b4263d74cd1ed570e9c0235c29c5a
Seisie <seisiep@msoe.edu>
2024-12-23 15:32:09 -0600

    intermediate: updated coustom plugin
    i should probably configure it later
    added a new formatter

--- lua/current-theme.lua
+++ lua/current-theme.lua
@@ -1,1 +1,1 @@
-vim.cmd("colorscheme absent-contrast")
+vim.cmd("colorscheme absent")
--- lua/lsps.lua
+++ lua/lsps.lua
@@ -2,14 +2,13 @@
 
 -- Define a table of LSP configurations
 local lspTable = {
-    "lua_ls",
-    "clangd"
+    "lua_ls", "clangd"
 }
 local onatatch = function(client, bufnr)
-	local opts = {noremap = true, silent = true}
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
 end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
@@ -25,4 +24,3 @@ for _, lsp in ipairs(lspTable) do
         }
     )
 end
-
