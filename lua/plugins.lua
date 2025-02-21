3081ecfbffb5e4d5ccbfcd7c19b837aa7c7ca4ce
paa(wsl_arch) <hfm598@gmail.com>
2025-02-20 23:29:29 -0600

    daily notes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -304,7 +304,27 @@ local plugins = {
     opts = {
       snippetDir = vim.fn.stdpath("config") .. "/snippets",
     }
+  }, {
+  'ntocampos/todone.nvim',
+  dependencies = {
+    -- Either one or the other
+    { "nvim-telescope/telescope.nvim", optional = true },
+    { "folke/snacks.nvim",             optional = true },
   },
+  opts = {
+    root_dir = vim.fn.stdpath("config") .. "/todone/",
+    float_position = "topright",
+  },
+  keys = {
+    { "<leader>tt", "<cmd>TodoneToday<cr>",       desc = "Open today's notes" },
+    { "<leader>tf", "<cmd>TodoneToggleFloat<cr>", desc = "Toggle priority float" },
+    -- The commands below require a picker
+    { "<leader>tl", "<cmd>TodoneList<cr>",        desc = "List all notes" },
+    { "<leader>tg", "<cmd>TodoneGrep<cr>",        desc = "Search inside all notes" },
+    { "<leader>tp", "<cmd>TodonePending<cr>",     desc = "List notes with pending tasks" },
+  }
+},
+
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
