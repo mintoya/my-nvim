118ea751f19966515e8b52d3860bde8d61ddc416
mintoya <hfm598@gmail.com>
2025-11-02 10:29:37 -0600

    pane commands

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -41,6 +41,13 @@ local keymaps = {
   { "n", "<leader>tk", ":above split | terminal<cr>:startinsert<cr>",      { desc = "open terminal up", silent = true } },
   { "n", "<leader>tj", ":belowright split | terminal<cr>:startinsert<cr>", { desc = "open terminal down", silent = true } },
   { "n", "<leader>th", ":leftabove vsplit|terminal<cr>:startinsert<cr>",   { desc = "open terminal left", silent = true } },
+
+  { "n", "<leader>p",  "",                                                 { desc = "open pane", silent = true } },
+  { "n", "<leader>pl", ":vsplit<cr>",                                      { desc = "open pane right", silent = true } },
+  { "n", "<leader>pk", ":above split<cr>",                                 { desc = "open pane up", silent = true } },
+  { "n", "<leader>pj", ":belowright split<cr>",                            { desc = "open pane down", silent = true } },
+  { "n", "<leader>ph", ":leftabove vsplit<cr>",                            { desc = "open pane left", silent = true } },
+
   { "n", "<leader>c",
     ":Pick colors<CR>",
     { desc = "change colorscheme", noremap = true, silent = true },
