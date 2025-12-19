cc0ca379f87ec9fc966609462917a6e3ec12d870
mintoya <hfm598@gmail.com>
2025-12-18 23:51:58 -0600

    blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,51 +1,51 @@
 local vim = vim
 return {
-  "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
-  opts_extend = { "sources.default" },
-
-  opts = {
-    cmdline = {
-      keymap = { preset = 'inherit' },
-      completion = { menu = { auto_show = true } },
-    },
-    keymap =
-    {
-      ["<C-e>"] = { "hide" },
-      ["<C-l>"] = { "select_and_accept" },
-
-      ["<C-k>"] = { "select_prev", "fallback" },
-      ["<C-j>"] = { "select_next", "fallback" },
-
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-      ["<Tab>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-    },
-    appearance = {
-      use_nvim_cmp_as_default = false,
-      nerd_font_variant = "mono",
-    },
-    completion = {
-      documentation = {
-        auto_show = true,
-        window = {
-          border = "rounded",
-        }
-      },
-    },
-    sources = {
-      providers = {
-        snippets = {
-          opts = {
-            search_paths = { vim.fn.stdpath("data") .. "/snippets" },
-          },
-        }
-      },
-
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-  },
-  event = "InsertEnter",
+  -- "saghen/blink.cmp",
+  -- dependencies = "rafamadriz/friendly-snippets",
+  -- opts_extend = { "sources.default" },
+  --
+  -- opts = {
+  --   keymap =
+  --   {
+  --     ["<C-e>"] = { "hide" },
+  --     ["<C-l>"] = { "select_and_accept" },
+  --
+  --     ["<C-k>"] = { "select_prev", "fallback" },
+  --     ["<C-j>"] = { "select_next", "fallback" },
+  --
+  --     -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+  --     -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+  --
+  --     ["<Tab>"] = { "snippet_forward", "fallback" },
+  --     ["<S-Tab>"] = { "snippet_backward", "fallback" },
+  --   },
+  --   cmdline = {
+  --     keymap = { preset = 'inherit' },
+  --     -- completion = { menu = { auto_show = true } },
+  --   },
+  --   appearance = {
+  --     use_nvim_cmp_as_default = false,
+  --     nerd_font_variant = "mono",
+  --   },
+  --   completion = {
+  --     documentation = {
+  --       auto_show = true,
+  --       window = {
+  --         border = "rounded",
+  --       }
+  --     },
+  --   },
+  --   sources = {
+  --     providers = {
+  --       snippets = {
+  --         opts = {
+  --           search_paths = { vim.fn.stdpath("data") .. "/snippets" },
+  --         },
+  --       }
+  --     },
+  --
+  --     default = { "lsp", "snippets", "buffer", "path", },
+  --   },
+  -- },
+  -- event = "InsertEnter",
 }
